fbccf5c76ca8adbc018246ff74fa0f16e0b129ee
“jeeva-sparkouttech” <jeevanantham@sparkouttech.com>
2023-06-12 11:14:06 +0530

    spider arguments

--- spiders/sample1.py
+++ spiders/sample1.py
@@ -37,14 +37,20 @@ class Sample1Spider(scrapy.Spider):
         # print(absolute_url)
 
         # next_page = response.xpath("//div[@class = 'mobile']/div/div/nav[@class='mainMenu']/ul[@class='nav1']").get()
-        next_page = response.css(
-            "div.mobile div div nav.mainMenu ul.nav1 li ul li a::attr(href)").getall()
-        
-        if next_page[1] is not None:
-            # yield response.follow('Apex_leadership', callback=self.apex_details)
-            # In the above code, response.follow() creates a new Request object to follow the link /Apex_leadership relative to the current response
-            # response.follow_all(): This method is similar to response.follow(), but it takes a list of links instead of a single link.
-            yield scrapy.Request(next_page[1], callback=self.apex_details)
+        # next_page = response.css(
+        #     "div.mobile div div nav.mainMenu ul.nav1 li ul li a::attr(href)").getall()
+
+        # if next_page[1] is not None:
+        # yield response.follow('Apex_leadership', callback=self.apex_details)
+        # In the above code, response.follow() creates a new Request object to follow the link /Apex_leadership relative to the current response
+        # response.follow_all(): This method is similar to response.follow(), but it takes a list of links instead of a single link.
+        # yield scrapy.Request(next_page[1], callback=self.apex_details)
+
+        # spider arguments "https://docs.scrapy.org/en/latest/intro/tutorial.html#using-spider-arguments"
+        url = "https://www.srmonline.in/" + getattr(self, 'tag', None)
+        print(url)
+        if url is not None:
+            yield scrapy.Request(url, callback=self.apex_details)
 
     def apex_details(self, response):
         filename = f"apex.html"
